24796720db7b65d0b63adbc5186cea808c7bbb0f
dobr1kq <165989825+dobr1kq@users.noreply.github.com>
2026-04-27 12:24:08 +0300

    fix

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Додайте цей рядок
+  images: {
+    unoptimized: true, // Потрібно для статичного експорту, якщо використовуєте <Image />
+  },
 };
 
 export default nextConfig;
